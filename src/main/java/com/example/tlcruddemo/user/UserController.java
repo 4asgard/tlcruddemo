70edab98559cae2a1330cebe7d31d18b7764461f
Agnieszka S <mail.sadowska@gmail.com>
2022-12-03 16:57:05 +0100

    Zajęcia - część 1 i 2

--- src/main/java/com/example/tlcruddemo/user/UserController.java
+++ src/main/java/com/example/tlcruddemo/user/UserController.java
@@ -1,5 +1,6 @@
 package com.example.tlcruddemo.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,6 +13,9 @@ import java.util.List;
 @RequestMapping("/user")
 public class UserController {
 
+    @Autowired
+    private UserRepository userRepository;
+
     @GetMapping("/info")
     public String getInfo(){
         System.out.println("Użytkownik próbuje pobrać infomacje o aplikacji");
@@ -22,13 +26,20 @@ public class UserController {
     public String getListOfUsers(Model model){
         List<User> users = new ArrayList<>();
         users.add(new User(1l));
-        users.add(new User(1l));
+        users.add(new User(2l));
         users.add(new User(3l, "agnieszka", "agnieszka@agnieszka.pl", true));
 
         model.addAttribute("users", users);
         System.out.println(users);
 
         return "usersList";
+    }
+
+    @GetMapping("/listFromDatabase")
+    public String usersFromDB(Model model){
+        List<UserEntity> users = userRepository.findAll();
+        System.out.println(users);
+        return "infoPage";
     }
 
 }
